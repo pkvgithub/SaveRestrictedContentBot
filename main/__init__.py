a472d201eb509b05942be41b059c4c5ddaea3b8c
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-06-29 07:37:20 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = "27803618" #config("API_ID", default=None, cast=int)
+API_HASH = "2cdaef9643189f6bd9c7b31a70257356" #config("API_HASH", default=None)
+BOT_TOKEN = "7048821409:AAF4foRNkVcRuKD5oXA8e2M2rQ_LIBjsiTg" #config("BOT_TOKEN", default=None)
+SESSION = "BQGoP-IAroAVGkkn2qUiMGxnsGBierCQIyLfahhUA9ci4b9vxFvSOCyQycQenEWzHWG0gbSpDaGfvHBs26fodHIjawuaGdOtAuE-yfHg-Qs6pOQyXNq7XfIBa0R2zC7YjDznodzSJ32_6Pw2uzf56p3OhLTKwifl6YsFvMh0F6oHKd--0vDRouy3Cg4kAEfu6Vd6uUFpJVY-JM0uufSj1MhHY6ULud5oekg8Sj8nopw99uD7mBFe28Rm-TiFt13MFRV6raxitbAyT-gGOMoiaBjgcwrfXhXTcbl8RfRbttd6edn8mZ768T0oTab-28lSu4SEMswfARuR9sNCHOSZvKATU84T1QAAAAAsaRVjAA" #config("SESSION", default=None)
+FORCESUB = "bot_user12838" #config("FORCESUB", default=None)
+AUTH = "745084259" #config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
